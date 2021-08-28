d295616ca1a5820ec6010db9dcda8327b4e7eea4
sshong11 <sshong2016@gmail.com>
2021-08-28 13:45:48 -0400

    made sticky nav

--- js/script.js
+++ js/script.js
@@ -49,4 +49,20 @@ function clear() {
     }
 }
 
-$('form').on('submit', handleGetData);
+
+window.onscroll = function() {
+    stickyNav()
+}
+
+var nav = document.querySelector("nav")
+var sticky = nav.offsetTop
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky")
+  }
+}
+
+$('form').on('submit', handleGetData)
